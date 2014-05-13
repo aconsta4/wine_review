f31f0182749ad6bb251fe9afe2ef56d53c32491e
Andrew Constantine <aconsta4@gmu.edu>
2014-05-13 18:46:51 -0400

    created a model for books

---
+++ db/migrate/20140513224625_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration
+  def change
+    create_table :books do |t|
+      t.string :name
+      t.integer :year
+      t.string :winery
+      t.string :country
+      t.string :varietal
+
+      t.timestamps
+    end
+  end
+end
